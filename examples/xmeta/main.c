f036c9313599de67ade9cda5a4c647e778ce8920
PublicHadyniak <public.hadyniak@gmail.com>
2016-04-23 23:44:22 +0300

    xmeta deserialization

--- examples/xmeta/main.c
+++ examples/xmeta/main.c
@@ -15,14 +15,14 @@ CMETA_STRUCT(CO_T, o_t, {
 
 XMETA_STRUCT(XO_T, CO_T, {
     XMETA(x, AUTO),
-    XMETA_ATTR(attr, AUTO),
+    XMETA(attr, AUTO),
 });
 
 typedef struct {
     bool b;
     int x;
     double y;
-    char * str;
+    char str[20];
     o_t o;
     o_t arr[3];
 } point_t;
@@ -31,13 +31,13 @@ CMETA_STRUCT(CPOINT_T, point_t, {
     CMETA(point_t, b, CBOOLEAN),
     CMETA(point_t, x, CINTEGER),
     CMETA(point_t, y, CDOUBLE),
-    CMETA_STR_PTR(point_t, str),
+    CMETA_STR(point_t, str, 20),
     CMETA_OBJ(point_t, o, CO_T),
     CMETA_ARR(point_t, arr, CO_T, 3),
 });
 
 XMETA_STRUCT(XPOINT_T, CPOINT_T, {
-    XMETA_ATTR(b, AUTO),
+    XMETA(b, AUTO),
     XMETA(x, AUTO),
     XMETA(y, AUTO),
     XMETA(str, AUTO),
@@ -45,8 +45,24 @@ XMETA_STRUCT(XPOINT_T, CPOINT_T, {
     XMETA_ARR(arr, XO_T, "item", AUTO),
 });
 
+
+
 int main(void) {
-    point_t point = { false, 5, 45.56, "Some string"};
+    point_t point = { false, 5, 45.56, "Some string", {6, 6}};
+    const char * xml = ""
+        "<root>"
+            "<b>true</b>"
+            "<x>23</x>"
+            "<y>100.001</y>"
+            "<str>String from XML</str>"
+            "<o><x>1</x><attr>2</attr></o>"
+            "<arr>"
+                "<item><x>3</x><attr>7</attr></item>"
+                "<item><x>5</x><attr>6</attr></item>"
+            "</arr>"
+        "</root>"
+    "";
+    xmeta_deserialize(&point, &XPOINT_T, xml);
     const char * str = xmeta_serialize(&point, &XPOINT_T);
     printf("%s\n", str);
     free((void *)str);
--- examples/xmeta/xmeta.c
+++ examples/xmeta/xmeta.c
@@ -61,12 +61,91 @@ const char * xmeta_serialize(void * obj, const xmeta_struct_t * xmeta) {
     _xmeta_serialize(obj, xmeta, (xmlNode *)xdoc, "root");
     xmlBuffer * bufferPtr = xmlBufferCreate();
 	xmlNodeDump(bufferPtr, NULL, (xmlNode *)xdoc, 0, 1);
-	char * strdup = malloc(sizeof(char) * strlen(bufferPtr->content) + 1);
-	strcpy(strdup, bufferPtr->content);
+	const char * content = (const char *)bufferPtr->content;
+	char * strdup = malloc(sizeof(char) * strlen(content) + 1);
+	strcpy(strdup, content);
 	xmlBufferFree(bufferPtr);
     return (const char *)strdup;
 }
 
+static xmlNode * _xml_getNodeByKey(xmlNode * xnode, const char * key) {
+    for (xmlNode * xI = xnode->children; xI; xI = xI->next) {
+        if(xmlStrcmp(xI->name, key) == 0) {
+            return xI;
+        }
+    }
+    return NULL;
+}
+
+static const char * _xml_getContentByKey(xmlNode * xnode, const char * key) {
+    xmlNode * xI = _xml_getNodeByKey(xnode, key);
+    if (NULL == xI) {
+        return NULL;
+    }
+    xmlChar * xvalue = xmlNodeGetContent(xI);
+    char * value = malloc(sizeof(char) * strlen((char *)xvalue) + 1);
+    strncpy(value, xvalue, strlen(xvalue) + 1);
+    return (const char *)value;
+}
+
+void _xmeta_deserialize(void * obj, const xmeta_struct_t * xmeta, xmlNode * xnode) {
+    for (int i = 0; i < xmeta->fieldsSize; i++) {
+        const xmeta_field_t * xfield = xmeta->fields + i;
+        const cmeta_field_t * cfield = cmeta_struct_getField(xmeta->metaType, xfield->fieldName);
+        if (NULL == cfield) {
+            // @todo error
+            return;
+        }
+        const char * xname = xfield->name;
+        if (cmeta_isObject(cfield)) {
+            xmlNode * innerNode = _xml_getNodeByKey(xnode, xname);
+            if (NULL == innerNode) {
+                // @todo
+                continue;
+            }
+            void * innerObj = cmeta_getObject(obj, xmeta->metaType, xfield->fieldName);
+            _xmeta_deserialize(innerObj, xfield->type, innerNode);
+            continue;
+        } else if (cmeta_isArray(cfield)) {
+            xmlNode * innerNode = _xml_getNodeByKey(xnode, xname);
+            if (NULL == innerNode) {
+                // @todo
+                continue;
+            }
+            size_t cArrSize = cmeta_getArraySize(xmeta->metaType, xfield->fieldName);
+            int index = 0;
+            for (xmlNode * xI = innerNode->children; xI && (index < cArrSize); xI = xI->next, index++) {
+                void * itemObj = cmeta_getArrayItem(obj, xmeta->metaType, xfield->fieldName, (int)index);
+                _xmeta_deserialize(itemObj, xfield->type, xI);
+            }
+        } else {
+            const char * content = _xml_getContentByKey(xnode, xname);
+            if (cmeta_type_eq(cfield->type, &CBOOLEAN)) {
+                bool value = (strncmp(content, "true", 4) == 0) ? true : false;
+                cmeta_setBoolean(obj, xmeta->metaType, xfield->fieldName, value);
+            } else if (cmeta_type_eq(cfield->type, &CINTEGER)) {
+                int value = atoi(content);
+                cmeta_setInteger(obj, xmeta->metaType, xfield->fieldName, value);
+            } else if (cmeta_type_eq(cfield->type, &CDOUBLE)) {
+                double value = atof(content);
+                cmeta_setDouble(obj, xmeta->metaType, xfield->fieldName, value);
+            } else if (cmeta_type_eq(cfield->type, &CSTRING)) {
+                const char * value = content;
+                cmeta_setString(obj, xmeta->metaType, xfield->fieldName, value);
+            }
+            free((void *)content);
+        }
+        if (xfield->isAttribute) {
+            //xmlNewProp(xnode, (unsigned char *)xname, (unsigned char *)xvalue);
+        } else {
+            //xmlNewChild(xnode, NULL, (unsigned char *)xname, (unsigned char *)xvalue);
+        }
+    }
+}
+
 void xmeta_deserialize(void * obj, const xmeta_struct_t * xtype, const char * xstr) {
-    // @todo
+    xmlDoc * xDoc = xmlReadMemory(xstr, strlen(xstr), NULL, NULL, 0);
+    xmlNode * xRoot = xmlDocGetRootElement(xDoc);
+    _xmeta_deserialize(obj, xtype, xRoot);
+    xmlFreeDoc(xDoc);
 }
