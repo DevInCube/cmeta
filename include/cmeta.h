ba427c827d7f7371644eee17cd20be37357569fb
PublicHadyniak <public.hadyniak@gmail.com>
2017-03-18 22:40:14 +0200

    move to c11

--- include/cmeta.h
+++ include/cmeta.h
@@ -7,11 +7,10 @@
 
 typedef struct cmeta_field_s cmeta_field_t;
 typedef struct cmeta_struct_s cmeta_struct_t;
-typedef const cmeta_struct_t * cmeta_type;
 
 struct cmeta_field_s {
 	const char *    name;
-	cmeta_type      type;
+	const cmeta_struct_t * type;
 	const size_t    offset;
 	const int       isPointer;
 	const size_t    arrSize;
@@ -27,8 +26,11 @@ struct cmeta_struct_s {
 #define __LEN(X) (sizeof(X)/sizeof(X[0]))
 #define __CMETA_FIELDS(CMETATYPENAME) __ ## CMETATYPENAME ## __fields
 
+#define CMETA_TYPE_VAR 	static const cmeta_struct_t
+#define CMETA_FIELD_VAR static const cmeta_field_t
+
 #define CMETA_TYPE(TYPENAME, TYPE)  \
-    static const cmeta_struct_t TYPENAME = {    \
+    CMETA_TYPE_VAR TYPENAME = {     \
         .name       = #TYPENAME,    \
         .size       = sizeof(TYPE), \
         .fields     = NULL,         \
@@ -36,8 +38,8 @@ struct cmeta_struct_s {
     }
 
 #define CMETA_STRUCT(CLASSNAME, TYPE, ...)						\
-	static const cmeta_field_t __CMETA_FIELDS(CLASSNAME)[] =	__VA_ARGS__;	\
-	static const cmeta_struct_t CLASSNAME = { 					\
+	CMETA_FIELD_VAR __CMETA_FIELDS(CLASSNAME)[] = __VA_ARGS__;	\
+	CMETA_TYPE_VAR CLASSNAME = { 								\
 		.name 		= #CLASSNAME,								\
 		.size 		= sizeof(TYPE),								\
 		.fields 	= __CMETA_FIELDS(CLASSNAME), 				\
@@ -70,31 +72,31 @@ CMETA_TYPE(CSTRING, char[1]);
 #define CMETA_OFFSET(PTR, OFFSET) ((void *)((char *)PTR + OFFSET))
 #define VALUE_POINTER(POINTER) ((void *)(*(intptr_t *)POINTER))
 
-void cmeta_struct_print(const cmeta_struct_t * metaClass);
-const char * cmeta_toString(const cmeta_struct_t * meta);
+void cmeta_struct_print(const cmeta_struct_t *metaClass);
+const char * cmeta_toString(const cmeta_struct_t *meta);
 
-int cmeta_type_eq(const cmeta_struct_t * a, const cmeta_struct_t * b);
+int cmeta_type_eq(const cmeta_struct_t *a, const cmeta_struct_t *b);
 int cmeta_isBasicType(const cmeta_field_t * field);
 int cmeta_isObject(const cmeta_field_t * field);
 int cmeta_isArray(const cmeta_field_t * field);
 
-const cmeta_field_t * cmeta_struct_getField(const cmeta_struct_t * meta, const char * fieldName);
+const cmeta_field_t * cmeta_struct_getField(const cmeta_struct_t *meta, const char * fieldName);
 
-const char * cmeta_getString(void * obj, const cmeta_struct_t * meta, const char * fieldName);
-void cmeta_setString(void * obj, const cmeta_struct_t * meta, const char * fieldName, const char * value);
+const char * cmeta_getString(void * obj, const cmeta_struct_t *meta, const char * fieldName);
+void cmeta_setString(void * obj, const cmeta_struct_t *meta, const char * fieldName, const char * value);
 
-bool cmeta_getBoolean(void * obj, const cmeta_struct_t * meta, const char * fieldName);
-void cmeta_setBoolean(void * obj, const cmeta_struct_t * meta, const char * fieldName, bool value);
+bool cmeta_getBoolean(void * obj, const cmeta_struct_t *meta, const char * fieldName);
+void cmeta_setBoolean(void * obj, const cmeta_struct_t *meta, const char * fieldName, bool value);
 
-int cmeta_getInteger(void * obj, const cmeta_struct_t * meta, const char * fieldName);
-void cmeta_setInteger(void * obj, const cmeta_struct_t * meta, const char * fieldName, int value);
+int cmeta_getInteger(void * obj, const cmeta_struct_t *meta, const char * fieldName);
+void cmeta_setInteger(void * obj, const cmeta_struct_t *meta, const char * fieldName, int value);
 
-double cmeta_getDouble(void * obj, const cmeta_struct_t * meta, const char * fieldName);
-void cmeta_setDouble(void * obj, const cmeta_struct_t * meta, const char * fieldName, double value);
+double cmeta_getDouble(void * obj, const cmeta_struct_t *meta, const char * fieldName);
+void cmeta_setDouble(void * obj, const cmeta_struct_t *meta, const char * fieldName, double value);
 
-void * cmeta_getObject(void * obj, const cmeta_struct_t * meta, const char * fieldName);
-void cmeta_setObject(void * obj, const cmeta_struct_t * meta, const char * fieldName, void * value);
+void * cmeta_getObject(void * obj, const cmeta_struct_t *meta, const char * fieldName);
+void cmeta_setObject(void * obj, const cmeta_struct_t *meta, const char * fieldName, void * value);
 
-int cmeta_getArraySize(const cmeta_struct_t * meta, const char * fieldName);
-void * cmeta_getArrayItem(void * obj, const cmeta_struct_t * meta, const char * fieldName, int index);
-void cmeta_setArrayItem(void * obj, const cmeta_struct_t * meta, const char * fieldName, int index, void * value);
+int cmeta_getArraySize(const cmeta_struct_t *meta, const char * fieldName);
+void * cmeta_getArrayItem(void * obj, const cmeta_struct_t *meta, const char * fieldName, int index);
+void cmeta_setArrayItem(void * obj, const cmeta_struct_t *meta, const char * fieldName, int index, void * value);
